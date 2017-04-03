fe6ffda51b4e9f3d7f8323af8470837b77482074
Mateusz Mikoś <Mateusz Miko>
2017-04-03 23:11:19 +0200

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,27 @@ $('#search').on('click', function() {
 
 function searchCities() {
 	var cityName = $('#city-name').val(),
-	appid = "&APPID=296f7a5708ace85d40abe8fe971314b8"
+	appid = "&APPID=296f7a5708ace85d40abe8fe971314b8",
+	units = "&units=metric",
+	language = "&lang=pl";
+
 
 	$.ajax({
-  		url: url + cityName + appid,
+  		url: url + cityName + units + language + appid,
   		method: 'GET',
   		success: showCityWeather
   	});
+
+  	$('.container').css('display', 'block');
 }
 
 function showCityWeather (resp) {
+
 	$('#cities').append('<p>' + resp.city.name + '</p>');
+
+		for (var i = 0; i < 40; i++) {
+		$('#day').append('<p>' + resp.list[i].dt_txt + ':</p>');
+		$('#temperature').append('<p>' + resp.list[i].main.temp + ' &degC</p>');
+		$('#windspeed').append('<p>' + resp.list[i].wind.speed + ' km/h</p>');
+	}
 }
